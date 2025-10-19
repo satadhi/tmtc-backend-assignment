491256669b2000cf9b1d3322ac0ae4d31c274ae5
satadhi <haldersatadhi@gmail.com>
2025-10-19 17:48:11 +0530

    add check for all env variable provided

--- src/server.js
+++ src/server.js
@@ -4,17 +4,52 @@ const connectDB = require('./config/db');
 const { connectRedis } = require('./config/redisClient');
 const logger = require('./config/logger');
 
+// Override console logs to use your logger
 console.log = (...args) => logger.info(args.join(' '));
 console.error = (...args) => logger.error(args.join(' '));
 
-const PORT = process.env.PORT || 4000;
+// List of required environment variables
+const REQUIRED_ENV = [
+  'PORT',
+  'MONGO_URI',
+  'JWT_SECRET',
+  'JWT_EXPIRES_IN',
+  'REDIS_URL',
+  'SHARE_BASE_URL',
+  'EMAIL_USER',
+  'EMAIL_PASS',
+  'EMAIL_FROM',
+  'NODE_ENV',
+  'SWAGGER_SERVER',
+];
+
+// Function to validate env vars
+function validateEnv() {
+  const missing = REQUIRED_ENV.filter((key) => !process.env[key]);
+  if (missing.length) {
+    console.error('Missing required environment variables:', missing.join(', '));
+    process.exit(1);
+  }
+}
+
+validateEnv();
+
+const PORT = process.env.PORT;
+
 (async () => {
   try {
+    // Connect to MongoDB
     await connectDB(process.env.MONGO_URI);
+    console.log('MongoDB connected');
+
+    // Connect to Redis
     await connectRedis();
-    app.listen(PORT, () => console.log(`Server up on port ${PORT}`));
+    console.log('Redis connected');
+
+    // Start Express server
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
   } catch (err) {
-    console.error(err);
+    console.error('Error starting server:', err);
     process.exit(1);
   }
 })();
